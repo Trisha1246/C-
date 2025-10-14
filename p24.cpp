6f274246b641e073c3929511cbb19a4db1f8b3b5
Trisha1246 <kolla.Trisha2006@gmail.com>
2025-10-14 22:47:42 +0530

    Add files via upload

---
+++ p24.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    virtual void show() {
+        cout << "Base class function" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    void show() {
+        cout << "Derived class function" << endl;
+    }
+};
+
+int main() {
+    Base* ptr;
+    Derived d;
+    ptr = &d;
+
+    ptr->show();
+
+    return 0;
+}
+
