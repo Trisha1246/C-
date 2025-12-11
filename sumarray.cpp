99aa29fc1126f8c4acf0fad8a611fecd507157c8
Trisha1246 <kolla.Trisha2006@gmail.com>
2025-12-11 18:50:17 +0530

    Add files via upload

---
+++ sumarray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int i,arr[n],sum=0;
+	for(i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	for(i=0;i<n;i++)
+	{
+		sum += arr[i];
+	}
+	cout<<sum;
+}
